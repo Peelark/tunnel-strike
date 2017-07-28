5e91ebec6191e9b2f7c28756967f371f45566952
Iain <iain.carson@gmail.com>
2017-07-28 20:41:42 +0100

    Initial commit. Player moves!

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    private Vector3 inputControl;
+
+    public float sensitivity = 3;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        controlPlayer();
+
+
+    }
+
+    private void controlPlayer()
+    {
+        inputControl = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"), 0f);
+        inputControl = inputControl * sensitivity;
+        rigidBody.AddForce(inputControl);
+    }
+}
